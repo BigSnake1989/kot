8e1ec16c5207f79590c1896530a2836ac0aa0de6
wanglei <wanglei@souche.com>
2016-09-08 09:49:14 +0800

    update page

---
+++ page/src/components/common/Post.jsx
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react';
+import MainLayout from '../../layouts/MainLayout/MainLayout.jsx';
+class Post extends Component {
+
+    render(){
+        return (
+            <MainLayout>
+                <div>
+                    hello post
+                </div>
+            </MainLayout>
+        )
+    }
+}
+
+export default Post;
--- page/src/routes/index.js
+++ page/src/routes/index.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react';
 import { Router, Route, IndexRoute, Link, hashHistory} from 'react-router';
 import App from '../components/App';
+import Post from '../components/common/Post.jsx';
 import NotFound from '../components/NotFound';
 
 const Routes = () =>
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/post" component={Post} />
     <Route path="*" component={NotFound}/>
   </Router>;
 
